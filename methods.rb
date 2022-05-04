6fdbeaf0630ff41a7b2b74bb0d6d5a4a55134f5c
sam9750 <sam.wadhwa95@gmail.com>
2022-05-03 20:26:51 -0400

    commit tree 56e662f61d2b197863d92fbd14a506363972bd6d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+require 'pry'
+
+def my_method(param)
+    binding.pry
+    puts "Running my_method"
+    param + 1
+  end
+
+def greet_programmer
+
+    puts "Hello, programmer!"
+end 
+
+def greet(naureen)
+    puts "Hello #{naureen}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+
+def add(num1, num2)
+    num1 + num2
+end 
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num/2
+end
